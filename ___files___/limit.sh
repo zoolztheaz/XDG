380e748c27197bf168c8d3cd38160b89fd22cdcb
zoolztheaz <54247099+zoolztheaz@users.noreply.github.com>
2024-01-09 22:15:27 +0700

    Update limit.sh

--- ___files___/limit.sh
+++ ___files___/limit.sh
@@ -3,9 +3,9 @@ wget -q -O /etc/systemd/system/limitvmess.service "${REPO}___files___/limitvmess
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}___files___/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}___files___/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}___files___/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}files/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}files/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}files/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}___files___/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}___files___/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}___files___/trojan" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.shadowsocks "${REPO}___files___/shadowsocks" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
